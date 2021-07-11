52d7364a5863b2304910440f571e5863320c7224
webdocs <56617924+Nikhil-pahal-js@users.noreply.github.com>
2021-07-11 12:00:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,13 +5,11 @@
     const mongoose = require("mongoose");
     const _ = require("lodash");
 
-
     // You can put your own content here: 
 
        // const homeStartingContent = "Your home Content"
        // const aboutContent = "Your about Content"
        // const contactContent = "Your contact info"
-
 
     const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac .";
     const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non di.";
