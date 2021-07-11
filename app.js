b39ad96590a9b13c8c71e2daa351a62604d7bf99
webdocs <56617924+Nikhil-pahal-js@users.noreply.github.com>
2021-07-11 11:56:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 
     // You can put your own content here: 
 
-     
+     const homeStartingContent = "Your home Content"
 
     const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac .";
     const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non di.";
